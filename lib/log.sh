3cfd757ad6464df4d77175dde095a5416f3032bf
Nimit Kalra <nimit@nimitkalra.com>
2015-12-23 14:34:46 -0600

    Add error logger.

--- lib/log.sh
+++ lib/log.sh
@@ -3,9 +3,13 @@ require "./util"
 require "colors"
 
 log() {
-  printf "$(colors cyan coral) %4s %12s : %s" "$1" "$(colors purple $2)" "$3"
+  printf "$(colors cyan coral) %s %20s %s\n" "$1" "$(colors purple $2) :" "$3"
 }
 
 info() {
   log "$(colors green info)" $1 $2
+}
+
+error() {
+  log "$(colors red 'ERR!')" "$1" "$2"
 }
