39be1a0aae8593e500d97656ce235c19b059bca0
Nimit Kalra <nimit@nimitkalra.com>
2015-12-25 01:30:47 -0600

    Add support for optional log prefix.

--- lib/log.sh
+++ lib/log.sh
@@ -2,21 +2,41 @@
 require "colors"
 
 general() {
-  printf "$(colors dark cyan 'coral') %s %s %s\n" "$1" "$(colors dark red "$2")" "$3"
+  if [ "$#" -eq 2 ]; then
+    printf "$(colors dark cyan 'coral') %s %s\n" "$1" "$(colors none "$2")"
+  else
+    printf "$(colors dark cyan 'coral') %s %s %s\n" "$1" "$(colors dark red "$2")" "$3"
+  fi
 }
 
 info() {
-  general "$(colors underline green 'info')" "$1" "$2"
+  if [ "$#" -eq 1 ]; then
+    general "$(colors underline green 'info')" "$1"
+  else
+    general "$(colors underline green 'info')" "$1" "$2"
+  fi
 }
 
 error() {
-  general "$(colors red 'ERR!')" "$1" "$2"
+  if [ "$#" -eq 1 ]; then
+    general "$(colors red 'ERR!')" "$1"
+  else
+    general "$(colors red 'ERR!')" "$1" "$2"
+  fi
 }
 
 warn() {
-  general $(printf "\e[0;30;43mWARN\e[0m") "$1" "$2"
+  if [ "$#" -eq 1 ]; then
+    general $(printf "\e[0;30;43mWARN\e[0m") "$1"
+  else
+    general $(printf "\e[0;30;43mWARN\e[0m") "$1" "$2"
+  fi
 }
 
 http() {
-  general "$(colors green 'http')" "$1" "$2"
+  if [ "$#" -eq 1 ]; then
+    general "$(colors green 'http')" "$1"
+  else
+    general "$(colors green 'http')" "$1" "$2"
+  fi
 }
