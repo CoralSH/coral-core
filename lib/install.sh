ed1d1e41c95a6fa941005098914bb693cd7292b3
Nimit Kalra <nimit@nimitkalra.com>
2016-01-03 22:36:52 -0600

    Add help for install command.

--- lib/install.sh
+++ lib/install.sh
@@ -2,6 +2,24 @@
 require "./log"
 require "colors"
 
+help() {
+  require "colors"
+
+  cat <<-EOF
+
+  » $(colors bold cyan "coral") $(colors green "install") $(colors red "[[<user>/]<package>[@<version>]]") $(colors yellow "[-g|--global]")
+  $(colors bright black "# install a package")
+
+  ┌ $(colors green "install")                   Locally install all dependencies in package.sh
+  ├ $(colors green "install") $(colors red "package")           Locally install $(colors red "package") from the registry
+  ├ $(colors green "install") $(colors red "package@0.1.3")     Locally install $(colors red "package v0.1.3") from the registry
+  ├ $(colors green "install") $(colors red "package") $(colors yellow "-g")        Globally install $(colors red "package") from the registry
+  ├ $(colors green "install") $(colors red "user/package")      Locally install $(colors red "user/package") from GitHub
+  └ $(colors green "install") $(colors red "user/package") $(colors yellow "-g")   Globally install $(colors red "user/package") from GitHub
+
+EOF
+}
+
 main() {
   local starting_dir="$1"
 
@@ -26,5 +44,3 @@ main() {
 
   log "info" "installed" "$2"
 }
-
-main
